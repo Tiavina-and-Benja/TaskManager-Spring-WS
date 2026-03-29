8450b34fed02b54e063942c10aad63ebfdcbc42e
Fanambony <ramarosonbenjamina@gmail.com>
2026-03-29 16:04:35 +0300

    CRUD TAG

---
+++ src/main/java/mg/itu/taskmanagerspringws/enums/TagStatus.java
@@ -0,0 +1,7 @@
+package mg.itu.taskmanagerspringws.enums;
+
+public enum TagStatus {
+    ACTIVE,
+    INACTIVE,
+    ARCHIVED
+}
---
+++ src/main/java/mg/itu/taskmanagerspringws/service/TagService.java
@@ -0,0 +1,44 @@
+package mg.itu.taskmanagerspringws.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import mg.itu.taskmanagerspringws.model.Tag;
+import mg.itu.taskmanagerspringws.enums.TagStatus;
+import mg.itu.taskmanagerspringws.repository.TagRepository;
+
+@Service
+public class TagService {
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    public List<Tag> getAllTags() {
+        return tagRepository.findAll();
+    }
+
+    public Tag getTagById(Long id) {
+        return tagRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Tag not found"));
+    }
+
+    public Tag createTag(Tag tag) {
+        return tagRepository.save(tag);
+    }
+
+    public Tag updateTag(Long id, Tag newTag) {
+        Tag tag = getTagById(id);
+        tag.setName(newTag.getName());
+        tag.setStatus(newTag.getStatus());
+        return tagRepository.save(tag);
+    }
+
+    public void deleteTag(Long id) {
+        tagRepository.deleteById(id);
+    }
+
+    public List<Tag> getByStatus(TagStatus status) {
+        return tagRepository.findByStatus(status);
+    }
+}

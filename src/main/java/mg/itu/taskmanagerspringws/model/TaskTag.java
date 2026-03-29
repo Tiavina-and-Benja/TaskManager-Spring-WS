b8f1f8176c1b5d647248ea0c53a0206be5bdd933
TiavinaMalalaniaina <tiavinaramia@gmail.com>
2026-03-29 19:03:13 +0300

    Add filter for Task

--- src/main/java/mg/itu/taskmanagerspringws/model/TaskTag.java
+++ src/main/java/mg/itu/taskmanagerspringws/model/TaskTag.java
@@ -21,6 +21,7 @@ public class TaskTag {
     private Long id;
 
     @ManyToOne
+    @JsonIgnore
     private Task task;
 
     @ManyToOne
--- src/main/java/mg/itu/taskmanagerspringws/repository/TaskRepository.java
+++ src/main/java/mg/itu/taskmanagerspringws/repository/TaskRepository.java
@@ -2,9 +2,10 @@ package mg.itu.taskmanagerspringws.repository;
 
 import mg.itu.taskmanagerspringws.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
     List<Task> findByProjectId(Long projectId);
 }

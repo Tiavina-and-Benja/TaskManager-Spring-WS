861dc4490a54166f3448a6f2391b0e4b3e643114
TiavinaMalalaniaina <tiavinaramia@gmail.com>
2026-03-30 16:08:31 +0300

    Add hateoas to TaskController

--- src/main/java/mg/itu/taskmanagerspringws/controller/TaskController.java
+++ src/main/java/mg/itu/taskmanagerspringws/controller/TaskController.java
@@ -4,12 +4,17 @@ import jakarta.validation.Valid;
 import mg.itu.taskmanagerspringws.dto.*;
 import mg.itu.taskmanagerspringws.service.TaskService;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -22,43 +27,100 @@ public class TaskController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<TaskDto> getTaskById(@PathVariable Long id) {
-        return ResponseEntity.ok(taskService.getTaskById(id));
+    public ResponseEntity<EntityModel<TaskDto>> getTaskById(@PathVariable Long id) {
+        TaskDto task = taskService.getTaskById(id);
+
+        EntityModel<TaskDto> model = EntityModel.of(task,
+                linkTo(methodOn(TaskController.class).getTaskById(id)).withSelfRel(),
+                linkTo(methodOn(TaskController.class).getTaskTags(id)).withRel("tags"),
+                linkTo(methodOn(TaskController.class).getTaskHistoryByTaskId(id)).withRel("history"),
+                linkTo(methodOn(TaskController.class).updateTask(id, null)).withRel("update"),
+                linkTo(methodOn(TaskController.class).deleteTask(id)).withRel("delete")
+        );
+
+        return ResponseEntity.ok(model);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<TaskDto> updateTask(@PathVariable Long id, @Valid @RequestBody TaskDto dto) {
-        return ResponseEntity.ok(taskService.updateTask(id, dto));
+    public ResponseEntity<EntityModel<TaskDto>> updateTask(@PathVariable Long id, @Valid @RequestBody TaskDto dto) {
+        TaskDto updatedTask = taskService.updateTask(id, dto);
+
+        EntityModel<TaskDto> model = EntityModel.of(updatedTask,
+                linkTo(methodOn(TaskController.class).getTaskById(id)).withSelfRel(),
+                linkTo(methodOn(TaskController.class).getTaskTags(id)).withRel("tags"),
+                linkTo(methodOn(TaskController.class).getTaskHistoryByTaskId(id)).withRel("history"),
+                linkTo(methodOn(TaskController.class).deleteTask(id)).withRel("delete")
+        );
+
+        return ResponseEntity.ok(model);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+    public ResponseEntity<EntityModel<String>> deleteTask(@PathVariable Long id) {
         taskService.deleteTask(id);
-        return ResponseEntity.noContent().build();
+
+        EntityModel<String> model = EntityModel.of("Task deleted",
+                linkTo(methodOn(TaskController.class).getTaskById(id)).withRel("self"), // optionnel, juste pour montrer
+                linkTo(methodOn(TaskController.class).getTaskHistoryByTaskId(id)).withRel("history")
+        );
+
+        return ResponseEntity.ok(model);
     }
 
     @GetMapping("/{id}/tag")
-    public ResponseEntity<List<TagDto>> getTaskTags(@PathVariable Long id) {
+    public ResponseEntity<CollectionModel<TagDto>> getTaskTags(@PathVariable Long id) {
         List<TagDto> tags = taskService.getTaskTag(id);
-        return ResponseEntity.ok(tags);
+
+        CollectionModel<TagDto> collection = CollectionModel.of(tags,
+                linkTo(methodOn(TaskController.class).getTaskById(id)).withRel("task"),
+                linkTo(methodOn(TaskController.class).addTagToTask(id, null)).withRel("add-tag")
+        );
+
+        return ResponseEntity.ok(collection);
     }
 
     @PostMapping("/{id}/tag")
-    public ResponseEntity<?> addTagToTask(@PathVariable Long id, @RequestBody Long tagId) {
-        taskService.addTagToTask(id, tagId);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    public ResponseEntity<EntityModel<TagDto>> addTagToTask(@PathVariable Long id, @RequestBody Long tagId) {
+        TagDto tag = taskService.addTagToTask(id, tagId);
+
+        EntityModel<TagDto> model = EntityModel.of(tag,
+                linkTo(methodOn(TaskController.class).getTaskTags(id)).withRel("tags"),
+                linkTo(methodOn(TaskController.class).getTaskById(id)).withRel("task"),
+                linkTo(methodOn(TaskController.class).removeTagToTask(id, tagId)).withRel("remove-tag")
+        );
+
+        return ResponseEntity.created(linkTo(methodOn(TaskController.class).getTaskTags(id)).toUri()).body(model);
     }
 
     @DeleteMapping("/{id}/tag")
-    public ResponseEntity<?> removeTagToTask(@PathVariable Long id, @RequestBody Long tagId) {
+    public ResponseEntity<EntityModel<String>> removeTagToTask(@PathVariable Long id, @RequestBody Long tagId) {
         taskService.removeTagFromTask(id, tagId);
-        return ResponseEntity.noContent().build();
+        EntityModel<String> model = EntityModel.of("Tag removed",
+                linkTo(methodOn(TaskController.class).getTaskTags(id)).withRel("tags"),
+                linkTo(methodOn(TaskController.class).getTaskById(id)).withRel("task"),
+                linkTo(methodOn(TaskController.class).addTagToTask(id, null)).withRel("add-tag")
+        );
+
+        return ResponseEntity.ok(model);
     }
 
     @GetMapping("/{id}/history")
-    public ResponseEntity<List<TaskHistoryResponseDto>> getTaskHistoryByTaskId(@PathVariable Long id) {
-        List<TaskHistoryResponseDto> taskHistories = this.taskService.getTaskHistoryByTaskId(id);
-        return ResponseEntity.ok(taskHistories);
+    public ResponseEntity<CollectionModel<EntityModel<TaskHistoryResponseDto>>> getTaskHistoryByTaskId(@PathVariable Long id) {
+
+        List<EntityModel<TaskHistoryResponseDto>> histories = taskService.getTaskHistoryByTaskId(id)
+                .stream()
+                .map(history -> EntityModel.of(history,
+                        linkTo(methodOn(TaskController.class).getTaskById(id)).withRel("task")
+                ))
+                .toList();
+
+        CollectionModel<EntityModel<TaskHistoryResponseDto>> collection = CollectionModel.of(histories,
+                linkTo(methodOn(TaskController.class).getTaskHistoryByTaskId(id)).withSelfRel(),
+                linkTo(methodOn(TaskController.class).getTaskById(id)).withRel("task"),
+                linkTo(methodOn(TaskController.class).getTaskTags(id)).withRel("tags")
+        );
+
+        return ResponseEntity.ok(collection);
     }
 
 }
